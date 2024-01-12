9d629aa2dbff8fb3f2767a96e66f6a1b2d7ffa0b
Viktor <yadelskyiviktor@gmail.com>
2024-01-12 12:45:37 +0200

    :sparkles:add placeholder data

---
+++ lib/data.ts
@@ -0,0 +1,92 @@
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+import corpcommentImg from '@/public/corpcomment.png';
+import rmtdevImg from '@/public/rmtdev.png';
+import wordanalyticsImg from '@/public/wordanalytics.png';
+
+export const links = [
+	{
+		name: 'Home',
+		hash: '#home',
+	},
+	{
+		name: 'About',
+		hash: '#about',
+	},
+	{
+		name: 'Projects',
+		hash: '#projects',
+	},
+	{
+		name: 'Skills',
+		hash: '#skills',
+	},
+	{
+		name: 'Experience',
+		hash: '#experience',
+	},
+	{
+		name: 'Contact',
+		hash: '#contact',
+	},
+] as const;
+
+export const experiencesData = [
+	{
+		title: 'Placeholder',
+		location: 'Placeholder',
+		description: 'Placeholder smt about school',
+		icon: React.createElement(LuGraduationCap),
+		date: '2019',
+	},
+	{
+		title: 'Placeholder',
+		location: 'Placeholder',
+		description: 'Placeholder',
+		icon: React.createElement(CgWorkAlt),
+		date: '2019 - 2021',
+	},
+	{
+		title: 'Placeholder',
+		location: 'Placeholder',
+		description: 'Placeholder',
+		icon: React.createElement(FaReact),
+		date: '2021 - present',
+	},
+] as const;
+
+//TODO: replace placeholder IMG
+
+export const projectsData = [
+	{
+		title: 'Placeholder',
+		description: 'Placeholder',
+		tags: ['React', 'Redux', 'SCSS'],
+		imageUrl: corpcommentImg, 
+	},
+	{
+		title: 'Placeholder',
+		description: 'Placeholder',
+		tags: ['React', 'TypeScript', 'Redux'],
+		imageUrl: rmtdevImg,
+	},
+	{
+		title: 'Placeholder',
+		description: 'Placeholder',
+		tags: ['React', 'TypeScript', 'Redux'],
+		imageUrl: wordanalyticsImg,
+	},
+] as const;
+
+export const skillsData = [
+	'HTML',
+	'SCSS',
+	'JavaScript',
+	'TypeScript',
+	'React',
+	'Next.js',
+	'Git',
+	'Redux',
+] as const;

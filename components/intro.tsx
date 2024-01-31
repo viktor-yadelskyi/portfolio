9047fd15929028ba9fff4a9061f000fc02e0d29b
Viktor <yadelskyiviktor@gmail.com>
2024-01-31 16:29:01 +0200

    :sparkles:add buttons to intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+import { HiDownload } from 'react-icons/hi';
 
 export default function Intro() {
 	return (
@@ -19,6 +24,48 @@ export default function Intro() {
 				building <span className="italic">sites & apps</span>. My focus is{' '}
 				<span className="underline">React</span>.
 			</motion.p>
+
+			<motion.div
+				className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium"
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.1,
+				}}
+			>
+				<Link
+					href="#contact"
+					className="group flex items-center bg-gray-900 text-white px-7 py-3 gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+				>
+					Contact me here{' '}
+					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+				</Link>
+
+				<a
+					className="group flex items-center bg-white px-7 py-3 gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+					href="/CV.pdf"
+					download
+				>
+					Dowholand CV{' '}
+					<HiDownload className="opacity-60 group-hover:translate-y-1  transition" />
+				</a>
+
+				<a
+					className="flex items-center bg-white p-4 gap-2 text-gray-700 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+					href="https://www.linkedin.com/in/viktor-yadelskyi/"
+					target="_blank"
+				>
+					<BsLinkedin />
+				</a>
+
+				<a
+					className="flex items-center bg-white p-4 gap-2 text-gray-700  text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+					href="https://github.com/viktor-yadelskyi"
+					target="_blank"
+				>
+					<FaGithubSquare />
+				</a>
+			</motion.div>
 		</section>
 	);
 }

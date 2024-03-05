030898ef6f7ef3383a842777561f8b0f85b1d008
Viktor <yadelskyiviktor@gmail.com>
2024-03-05 15:43:33 +0200

    :sparkles:add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default function Footer() {
 	return (
-		<footer>
-			<small>&copy; 2024 Viktor Yadelskyi. All rights reserved.</small>
-			<p>
-				<span>About this website:</span>build with React & Next.js, TypeScript,
-				Tailwind CSS, Framer Motion, github pages hosting.
+		<footer className="mb-10 px-4 text-center text-gray-500">
+			<small className="block mb-2 text-xs">
+				&copy; 2024 Viktor Yadelskyi. All rights reserved.
+			</small>
+			<p className="text-xs">
+				<span className="font-semibold">About this website:</span>build with
+				React & Next.js, TypeScript, Tailwind CSS, Framer Motion, github pages
+				hosting.
 			</p>
 		</footer>
 	);
